7ce487715ebc26efbdd355dacea079fd83080645
Atul <dr6217811@gmail.com>
2025-07-11 10:51:27 +0530

    Enhanced the UI of the main container of the Browse page

--- src/components/VideoBackGround.jsx
+++ src/components/VideoBackGround.jsx
@@ -7,8 +7,8 @@ const VideoBackground = ({ movieId }) => {
     if(!trailer) return;
 
     return <>
-        <div>
-            <iframe width="560" height="315" src={`https://www.youtube.com/embed/lyivgZ074PY?si=${trailer.key}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <div className=" " >
+            <iframe className=" w-full aspect-video " src={`https://www.youtube.com/embed/lyivgZ074PY?si=${trailer.key}?&autoplay=1&mute=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     </>
 }
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,10 +1,10 @@
 const VideoTitle = ({ original_title, overview }) => {
     return <>
-        <div className="pt-36 px-12 " >
+        <div className="w-full aspect-video pt-[18%] px-12 absolute text-white bg-gradient-to-r from-black" >
             <h1 className=" text-2xl font-bold " > {original_title} </h1>
             <p className=" py-6 text-md font-semibold w-1/4" > {overview} </p>
             <div>
-                <button className="bg-gray-500 text-white p-2 px-10 text-lg opacity-90 rounded-sm cursor-pointer" > ▶️ Play</button>
+                <button className="bg-white text-black font-semibold p-2 px-10 text-lg opacity-90 rounded-sm cursor-pointer hover:opacity-80 " > ▶️ Play</button>
                 <button className="mx-2 bg-gray-500 text-white p-2 px-10 text-lg opacity-90 rounded-sm cursor-pointer" >More Info</button>
             </div>
         </div>
