ac9e7e39b7200d389dce7fd449b9df99784c0897
Atul <dr6217811@gmail.com>
2025-07-13 15:46:32 +0530

    minor fixes

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -10,8 +10,8 @@ const VideoTitle = ({ original_title, overview }) => {
             <h1 className=" text-2xl font-bold " > {original_title} </h1>
             <p className="  md:py-6 text-md font-semibold md:w-1/4" > {overview} </p>
             <div>
-                <button onClick={() => dispatch(toggleAutoPlay())} className="bg-white text-black font-semibold p-1 px-8 text-lg opacity-90 rounded-sm cursor-pointer hover:opacity-80 " >{autoPlay ? "⏸️ Pause " : "▶️ Play"}</button>
-                <button onClick={() => dispatch(toggleMute())} className="mx-2 bg-gray-700 text-white py-1.5 px-8 text-lg opacity-90 rounded-sm cursor-pointer" >{mute ? "🔊 Unmute" : "🔇 Mute"}</button>
+                <button onClick={() => dispatch(toggleAutoPlay())} className="bg-white text-black font-semibold py-1 px-1 md:px-8 text-lg opacity-90 rounded-sm cursor-pointer hover:opacity-80 " >{autoPlay ? "⏸️ Pause " : "▶️ Play"}</button>
+                <button onClick={() => dispatch(toggleMute())} className="mx-2 bg-gray-700 text-white py-1.5 px-1.5 md:px-8 text-lg opacity-90 rounded-sm cursor-pointer" >{mute ? "🔊 Unmute" : "🔇 Mute"}</button>
             </div>
         </div>
     </>
